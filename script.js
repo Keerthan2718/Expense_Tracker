8f9a8ef2438cfcffa3faeeb735e20dd06798e2e4
Keerthan2718 <keerthan2718@gmail.com>
2026-07-19 17:36:10 +0530

    created expense object

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
+// Store all expenses
+const expenses = [];
+
 // Select the form
 const expenseForm = document.getElementById("expense-form");
 
 // Listen for form submission
 expenseForm.addEventListener("submit", function (event) {
 
-    // Prevent page refresh
     event.preventDefault();
 
     // Get input values
     const title = document.getElementById("title").value;
-    const amount = document.getElementById("amount").value;
+    const amount = Number(document.getElementById("amount").value);
     const category = document.getElementById("category").value;
     const date = document.getElementById("date").value;
 
-    // Print values
-    console.log({
-        title,
-        amount,
-        category,
-        date
-    });
+    // Create expense object
+    const expense = {
+        id: Date.now(),
+        title: title,
+        amount: amount,
+        category: category,
+        date: date
+    };
 
+    // Store expense in array
+    expenses.push(expense);
+
+    // Display array in console
+    console.log(expenses);
+
+    // Clear the form
+    expenseForm.reset();
 });
